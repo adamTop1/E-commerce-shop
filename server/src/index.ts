125cfe250df61f18543ba100f833b5363c6eddac
Adam <adamwasylak1@gmail.com>
2024-10-28 10:05:20 +0100

    init prisma

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,9 @@
-import express from 'express'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-const app = express()
 
-app.get('/', function (req, res) {
-	res.send('Hello World')
-})
+import app from './server'
 
 app.listen(3001, () => {
 	console.log('Server is running on http://localhost:3001');
   });
-
-
-
-
